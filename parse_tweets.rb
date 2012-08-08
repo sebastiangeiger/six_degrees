13417fa82069df636c70e3323137ce3e5d240af7
Sebastian Geiger <sebastian.geiger@gmail.com>
2012-08-08 22:43:30 +0200

    Renamed the value/key variables to something more expressive

--- parse_tweets.rb
+++ parse_tweets.rb
@@ -14,14 +14,14 @@ tweets.each do |line|
   #split the line at : to get the username and the tweet 
   #create the hash that holds the username as key and tweets in value
   #
-  hash_key = user_tweet[0]
-  hash_value = user_tweet[1]
+  twitter_handle = user_tweet[0]
+  tweet_body = user_tweet[1]
 
   #Build my hash of users and tweets
-  if users[hash_key]
-  	users[hash_key] << hash_value
+  if users[twitter_handle]
+  	users[twitter_handle] << tweet_body
   else
-  	users[hash_key] = [hash_value]
+  	users[twitter_handle] = [tweet_body]
   end
 end
 
@@ -30,8 +30,8 @@ end
 all_users = [] #create an array to hold all the instances of the User class to be used later 
 
 #break the hash key and value up, create a new User class and pass the key and value to them
-users.each do |key, value|
- user = User.new(key, value)
+users.each do |twitter_handle, all_his_tweets|
+ user = User.new(twitter_handle, all_his_tweets)
  all_users << user
  #puts user.user_name # need to figure out how to print out the users in alpabetical order 
  #puts user.tweets
